385158300130672ebe52eec0c543d4ac25e544d2
Keenan Olsen <keenanolsen101@gmail.com>
2016-09-17 19:40:08 +0000

    Added the catergories and article model and database table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,23 @@
 class ArticlesController < ApplicationController
 
     def index
+      @content_first = 'This is some sample text for my awesome online newspaper'
+      @content_second = 'This is some sample text for my awesome online newspaper'
     end
 
     def new
     end
 
+    def create
+    end
+
     def edit
+    end
+
+    def update
+    end
+
+    def show
     end
 
     def destroy
